0f2a7f756b6402469b2f9e48db684cde64819924
Maratя <sss8822@icloud.comm>
2024-07-20 00:41:41 +0300

    Created appearance of rows with enemies at random times

--- index.js
+++ index.js
@@ -189,7 +189,7 @@ class Grid {
 
 const player = new Player()
 const projectiles = []
-const grids = [new Grid()]
+const grids = []
 const keys = {
     a: {
         pressed: false
@@ -201,6 +201,9 @@ const keys = {
         pressed: false
     },
 }
+
+let frames = 0
+let randomInterval = Math.floor((Math.random() * 500) + 500)
 
 /**
  * The main game loop that updates and redraws the game.
@@ -249,6 +252,17 @@ function animate() {
         player.velocity.x = 0
         player.rotation = 0
     }
+
+    //console.log(frames)
+    // spawning enemies
+    if (frames % randomInterval  === 0) {
+        grids.push(new Grid())
+        randomInterval = Math.floor((Math.random() * 500) + 500)
+        frames = 0
+        console.log(randomInterval)
+    }
+
+    frames++
 }
 animate()
 
